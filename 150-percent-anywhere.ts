78f1301b83e1a1944298569abf621b643492e675
lideming <me@yuuza.net>
2023-10-18 19:52:34 +0800

    Update 150-percent-anywhere.ts

--- 150-percent-anywhere.ts
+++ 150-percent-anywhere.ts
@@ -1,48 +1,56 @@
-let sourceX = 0; // 0 for core
-let sourceY = 0;
-
-let destX = getLink(0).x;
-let destY = getLink(0).y;
-
 let item1 = Items.phaseFabric;
 let item2 = Items.silicon;
 let count = 20;
 
+const source = () => reachBuilding(getCore());
+// const source = () => reachXY(114, 514);
+const dest = () => reachBuilding(getLink(0));
 
-do {
-    unitBind(Units.poly);
-} while (!Vars.unit || Vars.unit.controlled);
-unitControl.flag(150);
+main();
 
-if (!sourceX && !sourceY) {
-    const [found, coreX, coreY] = unitLocate.building({ group: 'core', enemy: false });
-    if (!found) endScript();
-    sourceX = coreX;
-    sourceY = coreY;
-}
+function main() {
+    do {
+        unitBind(Units.poly);
+    } while (!Vars.unit || Vars.unit.controlled);
+    unitControl.flag(150);
 
-if (Vars.unit.totalItems > 0) {
-    unitControl.itemDrop(Blocks.air, 999);
-}
-
-let currentItem = item1;
-
-while (1) {
-    unitControl.itemTake(reachBuilding(sourceX, sourceY), currentItem, count);
-    while (Vars.unit.totalItems) {
-        checkAlive();
-        unitControl.itemDrop(reachBuilding(destX, destY), Vars.unit.totalItems);
+    if (Vars.unit.totalItems > 0) {
+        unitControl.itemDrop(Blocks.air, 999);
     }
-    checkAlive();
-    currentItem = currentItem == item1 ? item2 : item1;
+
+    let currentItem = item1;
+
+    while (1) {
+        checkAlive();
+        unitControl.itemTake(source(), currentItem, count);
+        while (Vars.unit.totalItems) {
+            checkAlive();
+            unitControl.itemDrop(dest(), Vars.unit.totalItems);
+        }
+        currentItem = currentItem == item1 ? item2 : item1;
+    }
 }
 
-function reachBuilding(x: number, y: number) {
+function reachXY(x: number, y: number) {
     while (!unitControl.within({ x, y, radius: 6 })) {
         checkAlive();
         unitControl.approach({ x, y, radius: 6 });
     }
     return unitControl.getBlock(x, y)[1];
+}
+
+function reachBuilding(building: AnyBuilding) {
+    const { x, y } = building;
+    while (!unitControl.within({ x, y, radius: 6 })) {
+        checkAlive();
+        unitControl.approach({ x, y, radius: 6 });
+    }
+    return building;
+}
+
+function getCore() {
+    const [, , , building] = unitLocate.building({ group: 'core', enemy: false });
+    return building;
 }
 
 function checkAlive() {
