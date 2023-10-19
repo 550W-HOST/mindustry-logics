0e4705e61fb237093562a7d1566d02080c425eeb
PaperCube <imzhy@hotmail.com>
2023-10-19 11:32:03 +0100

    update keyboard shortcut

--- mlogjs-editor-ui/keyboard-shortcut.js
+++ mlogjs-editor-ui/keyboard-shortcut.js
@@ -3,14 +3,14 @@
 // @version      0.1
 // @description  As stated in title
 // @author       You
-// @match        *://mlogjs.github.io/mlogjs/editor.html
+// @match        *://mlogjs.github.io/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     document.addEventListener('keydown', function (e) {
-        if(e.ctrlKey && e.code == 'KeyS') {
+        if(document.location.href.indexOf('editor') != -1 && e.ctrlKey && e.code == 'KeyS') {
             document.getElementsByClassName("bar-actions")[0].getElementsByTagName("button")[2].click()
             e.preventDefault()
             return false
