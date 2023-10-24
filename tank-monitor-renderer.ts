573dcdaa8fb72d2a820d1d4ae64efe3da2181e2a
PaperCube <imzhy@hotmail.com>
2023-10-24 02:16:12 +0100

    storage monitor: can display content type of storage

--- tank-monitor-renderer.ts
+++ tank-monitor-renderer.ts
@@ -16,9 +16,14 @@ const enum MemoryFields {
     CONTAINERS_COUNT,
     TOTAL_LIQUIDS,
     TOTAL_LIQUID_CAPACITY,
+    SUBSTANCE_TYPE,
     STRUCT_SIZE = 12,
     BASE = 8,
 }
+
+const FLAG_LIQUID = 1 << 29;
+const FLAG_ITEM = 1 << 28;
+const ID_MASK = 0xFFFF
 
 const backendDeltaSmoothingSampleCount = 5;
 
@@ -95,12 +100,12 @@ function mainLoop() {
     } else {
         draw.clear(90, 90, 90)
     }
-    drawIcon()
 
     copyMem(MemoryFields.BASE + MemoryFields.DELTA, MemoryFields.BASE + MemoryFields.DELTA, MemoryFields.STRUCT_SIZE)
     copyMem(MemoryFields.BASE + MemoryFields.TOTAL_LIQUIDS, MemoryFields.BASE + MemoryFields.TOTAL_LIQUIDS, MemoryFields.STRUCT_SIZE)
     copyMem(MemoryFields.BASE + MemoryFields.TOTAL_LIQUID_CAPACITY, MemoryFields.BASE + MemoryFields.TOTAL_LIQUID_CAPACITY, MemoryFields.STRUCT_SIZE)
     copyMem(MemoryFields.BASE + MemoryFields.TIMESTAMP, MemoryFields.BASE + MemoryFields.TIMESTAMP, MemoryFields.STRUCT_SIZE)
+    copyMem(MemoryFields.BASE + MemoryFields.SUBSTANCE_TYPE, MemoryFields.BASE + MemoryFields.SUBSTANCE_TYPE, MemoryFields.STRUCT_SIZE)
     printFlush()
 
     warningLevel = 0;
@@ -129,12 +134,14 @@ function drawStats(i) {
             x2: 5 + barHeight,
             y2: y0 + barHeight,
         })
+        drawIcon(i, undefined)
         return
     }
 
     const delta = mem[baseAddr + MemoryFields.DELTA]
     const liquids = mem[baseAddr + MemoryFields.TOTAL_LIQUIDS]
     const liquidCapacity = mem[baseAddr + MemoryFields.TOTAL_LIQUID_CAPACITY]
+    const substanceType = mem[baseAddr + MemoryFields.SUBSTANCE_TYPE]
 
     const liquidRatio = liquids / liquidCapacity
     const barWidth = (liquidRatio) * D
@@ -179,6 +186,27 @@ function drawStats(i) {
         height: barHeightHalf,
         width: barDeltaWidth60s,
     })
+
+    var image = undefined;
+    if (substanceType) {
+        if ((substanceType & FLAG_ITEM)) {
+            image = lookup.item(substanceType & ID_MASK)
+        } else if ((substanceType & FLAG_LIQUID)) {
+            image = lookup.liquid(substanceType & ID_MASK)
+        }
+    }
+
+    // if (image) {
+        // draw.image({
+        //     x: barHeightHalf + 3,
+        //     y: y0 + barHeightHalf,
+        //     image: image,
+        //     size: barHeight + barSpacing / 2,
+        //     rotation: 0
+        // })
+    // }
+    print`[blue]drawIcon(${i}, ${image})\n[white]`
+    drawIcon(i, image)
 
     if (liquidRatio < 0.1) {
         draw.color(240, 0, 0)
@@ -228,22 +256,31 @@ function haltUntilConfiguredLinksChange() {
     endScript()
 }
 
-function drawIcon() {
-    draw.color(255, 255, 255, 255)
-    draw.image({
-        x: 17,
-        y: D - 17,
-        image: Liquids.water,
-        size: 15,
-        rotation: 0
-    })
-    draw.image({
-        x: 32,
-        y: D - 17,
-        image: Items.copper,
-        size: 15,
-        rotation: 0
-    })
+function drawIcon(pos, image) {
+    print`[red]image = ${image}[white]\n`
+    var px = 10 + pos * 15
+    var py = D - 17
+    var sz = 15
+    var halfSz = sz / 2
+    if (image) {
+        draw.color(255, 255, 255, 255)
+        draw.image({
+            x: px,
+            y: py,
+            image: image,
+            size: sz,
+            rotation: 0
+        })
+    } else {
+        draw.color(140, 140, 140)
+        draw.stroke(2)
+        draw.lineRect({
+            x: px - halfSz,
+            y: py - halfSz,
+            width: sz,
+            height: sz
+        })
+    }
 }
 
 function drawWarning() {
@@ -290,6 +327,7 @@ function runBackend() {
     var latestSampleValue = 0;
     var oldestSampleTimestamp = 0;
     var latestSampleTimestamp = 1e9;
+    var previousSubstanceId = 0;
 
     var itemCategoryOfInterestSorter = getFirst(Blocks.sorter) ?? getFirst(Blocks.invertedSorter) ?? getFirst(Blocks.unloader);
 
@@ -324,24 +362,65 @@ function runBackend() {
         return (latestSampleValue - oldestSampleValue) / (latestSampleTimestamp - oldestSampleTimestamp) * 1000
     }
 
+    function getLiquidIdFromLiquidContainer(liquidContainer) {
+        var maxStorage = 0;
+        var res = 0;
+        for (var ti = 0; ti < Vars.liquidCount; ti++) {
+            var curStorage = liquidContainer[lookup.liquid(ti)];
+            if (curStorage > maxStorage) {
+                maxStorage = curStorage;
+                res = ti | FLAG_LIQUID;
+            }
+        }
+        return res;
+    }
+
+    function getItemIdFromItemType(itemType) {
+        for (var ti = 0; ti < Vars.itemCount; ti++) {
+            if (lookup.item(ti) == itemType) {
+                return ti | FLAG_ITEM;
+            }
+        }
+        return 0;
+    }
+
 
     function main() {
         var totalLiquids = 0
         var totalCapacity = 0
         var delta = 0
+        var substanceType = 0;
+
         for (var i = 0; i < Vars.links; i++) {
             const building = getLink(i)
             if (building.type == Blocks.liquidContainer || building.type == Blocks.liquidTank) {
                 totalLiquids += building.totalLiquids
                 totalCapacity += building.liquidCapacity
+                if (!substanceType) {
+                    substanceType = getLiquidIdFromLiquidContainer(building);
+                }
             } else if (building.type == Blocks.container || building.type == Blocks.vault) {
                 if (itemCategoryOfInterestSorter?.config !== undefined) {
-                    totalLiquids += building[itemCategoryOfInterestSorter.config]
+                    const itemConfig = itemCategoryOfInterestSorter.config
+                    totalLiquids += building[itemConfig]
+                    if (!substanceType) {
+                        substanceType = getItemIdFromItemType(itemConfig);
+                    }
                 } else {
                     totalLiquids += building.totalItems
+                    if (!substanceType && building.totalItems != 0) {
+                        substanceType = getItemIdFromItemType(building.firstItem);
+                    }
                 }
                 totalCapacity += building.itemCapacity
             }
+        }
+
+        // print`substanceType = raw: ${substanceType} (id: ${substanceType & ID_MASK}): ${lookup.liquid(substanceType & ID_MASK)} or ${lookup.item(substanceType & ID_MASK)}`
+        // printFlush()
+
+        if (substanceType) {
+            previousSubstanceId = substanceType
         }
 
         // print("liquids:", previousTotalLiquids, "->", totalLiquids, ", in ", Vars.time - previousDataUpdatedAt, "\n")
@@ -357,5 +436,6 @@ function runBackend() {
         mem[baseAddr + MemoryFields.TOTAL_LIQUIDS] = totalLiquids
         mem[baseAddr + MemoryFields.TOTAL_LIQUID_CAPACITY] = totalCapacity
         mem[baseAddr + MemoryFields.TIMESTAMP] = Vars.time
+        mem[baseAddr + MemoryFields.SUBSTANCE_TYPE] = previousSubstanceId
     }
-}
+} 
