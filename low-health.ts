e6701d4940864b2285cad8b4ea600e99216179aa
PuQing <me@puqing.work>
2025-10-18 13:22:33 +0800

    Update low-health.ts

--- low-health.ts
+++ low-health.ts
@@ -1,9 +1,13 @@
+let lastUnitCheck = 0;
+let unitCheckFail = false;
+
 main();
 
 function main() {
     while (1) {
         unitBind(Units.mega);
-        if ((Vars.unit.health / Vars.unit.maxHealth) < 0.5) {
+        checkAlive();
+        if ((Vars.unit.health / Vars.unit.maxHealth) < 1.2) {
             const { x, y } = getRepair();
             while (!unitControl.within({ x, y, radius: 5 })) { 
                 unitControl.approach({ x, y, radius: 5 });
@@ -12,6 +16,7 @@ function main() {
         }
         unitControl.unbind();
         unitBind(Units.poly);
+        checkAlive();
         if ((Vars.unit.health / Vars.unit.maxHealth) < 0.5) {
             const { x, y } = getRepair();
             while (!unitControl.within({ x, y, radius: 5 })) {
@@ -20,10 +25,27 @@ function main() {
             wait(0.1);
         }
         unitControl.unbind();
+        wait(1);
     }
 }
 
 function getRepair() { 
     const [, , , building] = unitLocate.building({ group: 'repair', enemy: false });
     return building;
+}
+
+function checkAlive() {
+    let unitExists = Vars.unit;
+    let unitDead = Vars.unit.dead;
+    let unitControlled = Vars.unit.controlled;
+    let unitController = Vars.unit.controller;
+    let unitControlByThis = unitController === Vars.this;
+    unitCheckFail = unitExists == undefined ||
+        unitDead ||
+        (unitControlled && unitControlled != ControlKind.ctrlProcessor) ||
+        (unitControlled && !unitControlByThis);
+    lastUnitCheck = Vars.time;
+    if (unitCheckFail) {
+        endScript();
+    }
 }
